addf03705c0de52db018af0888a3ed612a1ee213
lennwouh <leyuma256@gmail.com>
2022-06-20 15:50:52 +0200

    Initial Commit

---
+++ Shared/tanken_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  tanken_appApp.swift
+//  Shared
+//
+//  Created by Lennox Marten on 20.06.22.
+//
+
+import SwiftUI
+
+@main
+struct tanken_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
